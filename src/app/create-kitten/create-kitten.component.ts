af8fc583b0f57f559e8c98920f952ddd945bb3cc
Elodie <elodie.dargot@gmail.com>
2023-09-29 17:58:07 +0200

    correction flexbox et message d'erreur

--- src/app/create-kitten/create-kitten.component.ts
+++ src/app/create-kitten/create-kitten.component.ts
@@ -12,28 +12,37 @@ export class CreateKittenComponent implements OnInit {
 
   kittenForm!: FormGroup
   click!: boolean
+  showErrorMessage!: boolean
 
   constructor(private formBuilder: FormBuilder,
               private kittenService: KittenService,
               private routeur: Router){}
 
   ngOnInit(): void {
-      this.kittenForm = this.formBuilder.group({
-        name: [null, [Validators.required]],
-        breed: [null, [Validators.required]],
-        birthday: [null, [Validators.required]],
-        picture: [null],
-        isAdopted: [false]
-      })
-    }
+
+    this.showErrorMessage = false
+
+    this.kittenForm = this.formBuilder.group({
+      name: [null, [Validators.required]],
+      breed: [null, [Validators.required]],
+      birthday: [null, [Validators.required]],
+      picture: [null],
+      isAdopted: [false]
+    })
+  }
 
 
   onSubmit(){
-    this.kittenService.addKitten(this.kittenForm.value)
-    console.log(this.kittenForm.value)
-    this.click = true
-    setTimeout(() =>{
-      this.routeur.navigateByUrl('kittenCollection')
-    }, 2000);
+    if (this.kittenForm.valid){
+      console.log("formulaire valide")
+      this.kittenService.addKitten(this.kittenForm.value)
+      this.click = true
+      setTimeout(() =>{
+        this.routeur.navigateByUrl('kittenCollection')
+      }, 2000)
+    } else {
+      console.log("formulaire invalide")
+      this.showErrorMessage = !this.showErrorMessage
+    }
   }
 }
